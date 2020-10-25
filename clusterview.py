888323b139b2471bc604a41a7a72ee42c5bcf118
epolnovi <e.pol@novi-education.nl>
2020-10-25 02:10:30 +0200

    Exceptions split

--- clusterview.py
+++ clusterview.py
@@ -5,6 +5,11 @@ import datetime
 configfile="config.json"
 logfile="clusterview.log"
 
+#class Environment:
+#    def __init__(self, environment, cluster_name,administrator,vip_address,vip_fqdn,nodes):
+
+# Function to log messages to a logfile
+
 def log_output(logstring):
     try:
         f=open(logfile,'a')
@@ -15,10 +20,11 @@ def log_output(logstring):
         print('Can not write to logfile !')
         print(str(e))
 
+# import settings from config.json
 
 try:
     with open(configfile, encoding='utf-8') as config_json:
-        cluster_config = json.load(config_obj)
+        cluster_config = json.load(config_json)
 except NameError:
     log_output('Can not parse json in '+configfile)
     sys.exit()
@@ -26,12 +32,16 @@ except OSError as e:
     log_output(str(e))
     sys.exit()
 
+# build objects from json config
 
+for cluster_site, cluster_environment in cluster_config.items():
+    cluster=cluster_environment
+    for c,d in cluster.items():
+        print ("site:"+cluster_site)
+        print ('environment:'+str(cluster))
+        print ('c'+c)
+        print ('d'+ str(d))
 
-
-
-
-print (cluster_config)
 
 
 
