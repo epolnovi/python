9a973514bd1cadff626e011342bada5599a1c555
epolnovi <e.pol@novi-education.nl>
2020-11-07 22:44:58 +0100

    heel stuk opgeschoten

--- clusterview.py
+++ clusterview.py
@@ -1,10 +1,117 @@
+import datetime
 import json
 import sys
-import datetime
+import pssh
 
 configfile="config.json"
 logfile="clusterview.log"
 
+class Site:
+
+    def __init__(self,sitename,clusters):
+        self.sitename=sitename
+        self.clusters=clusters
+
+class Cluster:
+
+    def __init__(self,cluster_name,administrator,designated_coordinator, nodes, cib=""):
+        self.cluster_name=cluster_name
+        self.administrator=administrator
+        self.designated_coordinator=designated_coordinator
+        self.nodes=nodes
+        self.cib=cib
+
+        @property
+        def cib(self):
+            return self.__cib
+
+        @cib.setter
+        def cib (self, cib_value):
+            self.cib=cib_value
+
+        def update_cib(self):
+            self.cib=
+
+            pass
+
+
+
+
+class Node:
+
+    def __init__(self, node_name, node_ipaddress, node_fqdn, ssh_keyfile,ssh_user,ssh_passwdfile,ssh_port, node_status="unknown"):
+        self.node_name=node_name
+        self.node_ipaddress=node_ipaddress
+        self.node_fqdn=node_fqdn
+        self.ssh_keyfile=ssh_keyfile
+        self.ssh_user=ssh_user
+        self.ssh_passwdfile=ssh_passwdfile
+        self.ssh_port=ssh_port
+        self.node_status=node_status
+
+        @property
+        def node_name(self):
+            return self.__node_name
+
+        @node_name.setter
+        def node_name (self, node_name_value):
+            self.node_name=node_name_value
+
+        @property
+        def node_ipaddress(self):
+            return self.__node_ipaddress
+
+        @node_ipaddress.setter
+        def node_ipaddress (self, node_ipaddress_value):
+            self.node_name=node_ipaddress_value
+
+        @property
+        def node_fqdn(self):
+            return self.__node_fqdn
+
+        @node_fqdn.setter
+        def node_fqdn (self, node_fqdn_value):
+            self.node_fqdn=node_fqdn_value
+
+        @property
+        def ssh_keyfile(self):
+            return self.__ssh_keyfile
+
+        @ssh_keyfile.setter
+        def ssh_keyfile (self, ssh_keyfile_value):
+            self.ssh_keyfile=ssh_keyfile_value
+
+        @property
+        def ssh_user(self):
+            return self.__ssh_user
+
+        @ssh_user.setter
+        def ssh_user (self, ssh_user_value):
+            self.ssh_user=ssh_user_value
+
+        @property
+        def ssh_passwdfile(self):
+            return self.__ssh_passwdfile
+
+        @ssh_passwdfile.setter
+        def ssh_passwdfile (self, ssh_passwdfile_value):
+            self.ssh_passwdfile=ssh_passwdfile_value
+
+        @property
+        def ssh_port(self):
+            return self.__ssh_port
+
+        @ssh_port.setter
+        def ssh_port(self, ssh_port_value):
+            self.ssh_port=self.sshport_value
+
+        @property
+        def node_status(self):
+            return self.__node_status
+
+        @node_status.setter
+        def node_status(self, ssh_port_value):
+            self.node_status=self.node_status
 #class Environment:
 #    def __init__(self, environment, cluster_name,administrator,vip_address,vip_fqdn,nodes):
 
@@ -32,24 +139,47 @@ except OSError as e:
     log_output(str(e))
     sys.exit()
 
-# build objects from json config
+configuration=cluster_config['configuration']
+privatekey=configuration['global_private_key_file']
 
-for k,v in cluster_config.items():
-    cluster_site=k
-    cluster_environment=v
-    for k,v in cluster_environment.items():
-        cluster=k
-        cluster_properties=v
-        cluster_name=cluster_properties["name"]
-        cluster_administrator=cluster_properties["name"]
-        cluster_vipaddress=cluster_properties["vipaddress"]
-        cluster_vipfqdn=cluster_properties["vipfqdn"]
-        cluster_nodes=cluster_properties["nodes"]
-        for k,v in cluster_nodes.items():
-            node_name=k
-            node_properties=v
-            node_ipaddress=node_properties["ipaddress"]
-            node_fqdn=node_properties["fqdn"]
-            node_sshkey=node_properties["sshkey"]
-            node_sshuser=node_properties["sshuser"]
-            print (cluster_site, cluster_vipaddress,node_ipaddress,node_fqdn)
+
+sites=cluster_config['sites']
+
+sitelist=[]
+
+for site in cluster_config['sites']:
+
+    clusterlist=[]
+
+    for cluster in site['clusters']:
+
+        nodelist=[]
+
+        for node in cluster['nodes']:
+            nodelist.append(Node(
+                node_name=node['nodename'],
+                node_ipaddress=node['ipaddress'],
+                node_fqdn=node['fqdn'],
+                ssh_keyfile=node['ssh_keyfile'],
+                ssh_user=node['ssh_user'],
+                ssh_passwdfile=node['ssh_passwdfile'],
+                ssh_port=node['ssh_port']
+            ))
+
+
+
+        clusterlist.append(Cluster(
+            cluster_name=cluster['clustername'],
+            administrator=cluster['administrator'],
+            designated_coordinator=cluster['dc'],
+            nodes=nodelist
+        ))
+
+    sitelist.append(Site(
+        sitename=site['sitename'],
+        clusters=clusterlist
+    ))
+
+for site in sitelist:
+    for cluster in site.clusters:
+        print(cluster.designated_coordinator)
